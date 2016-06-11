4522c38f4252cb117b89649161e68b349ba75fb5
James <jamesbrad1309@gmail.com>
2016-06-11 15:58:27 +0700

    Basic setup Core Class

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+function __autoload($class)
+{
+    if (substr($class,0,3) !== 'CI_') {
+        if (file_exists($file = APPPATH . 'core/' . $class . '.php')) {
+            include $file;
+        }
+    }
+}
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends frontend_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -26,6 +26,7 @@ class Welcome extends CI_Controller {
 		{
 			show_error($this->migration->error_string());
 		}
+
 		$this->load->view('welcome_message');
 	}
 }
