666e6dcb5ebffdf06600247e9f198969dfe717d1
James <jamesbrad1309@gmail.com>
2016-06-12 15:44:45 +0700

    Litte tweak on MY_Controller

--- application/core/frontend_Controller.php
+++ application/core/frontend_Controller.php
@@ -1,5 +1,5 @@
 <?php
-
+    // define('BASEPATH') OR exit('No direct script access allowed');
     class frontend_Controller extends MY_Controller{
         public function __construct(){
             parent::__construct();
